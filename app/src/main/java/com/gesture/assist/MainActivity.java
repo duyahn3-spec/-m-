c8e00fd5653d793289817cd6de3dc6d35db1a694
duyahn3-spec <duyahn3@gmail.com>
2026-07-25 22:09:38 +0700

    Update MainActivity.java

--- app/src/main/java/com/gesture/assist/MainActivity.java
+++ app/src/main/java/com/gesture/assist/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Cần file activity_main.xml
+        setContentView(R.layout.activity_main); // PHẢI CÓ FILE XML NÀY
 
         prefs = getSharedPreferences("gamepad_settings", MODE_PRIVATE);
 
@@ -50,7 +50,7 @@ public class MainActivity extends Activity {
             }
             Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
             startActivity(intent);
-            Toast.makeText(this, "🎮 Địt mẹ mày suku chưa¿🤕!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Địt mẹ lọ đi 💦!", Toast.LENGTH_LONG).show();
         });
     }
 }
